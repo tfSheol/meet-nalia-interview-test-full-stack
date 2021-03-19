92bdf08c85d865a0d4bf55d2c26c49fc53fb710b
Teddy Fontaine <tf.sheol@gmail.com>
2021-03-19 10:43:09 +0100

    add achievements folder + fix maticon lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { MatButtonModule } from '@angular/material/button';
     AppRoutingModule,
     CoreModule,
     BrowserAnimationsModule,
+    MatIconModule,
     MatFormFieldModule,
     MatInputModule,
     MatDialogModule,
